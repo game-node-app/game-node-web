93a98935e0f61bb58ab740e6f8c9bbf4ed79c48e
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-03-12 22:52:04 -0300

    fix incorrect user signout (probably)

--- src/pages/auth/logout.tsx
+++ src/pages/auth/logout.tsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { signOut } from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import Session from "supertokens-auth-react/recipe/session";
+import { useQueryClient } from "@tanstack/react-query";
+import CenteredLoading from "@/components/general/CenteredLoading";
 
 const Logout = () => {
+    const queryClient = useQueryClient();
     const router = useRouter();
     useEffect(() => {
-        signOut().then(() => {
-            router.push("/");
-        });
-    }, [router]);
-    return <div></div>;
+        let ignore = false;
+        (async () => {
+            if (ignore) return;
+            await Session.signOut();
+            await queryClient.invalidateQueries();
+            await router.push("/");
+        })();
+        return () => {
+            ignore = true;
+        };
+    }, [queryClient, router]);
+    return <CenteredLoading></CenteredLoading>;
 };
 
 export default Logout;
